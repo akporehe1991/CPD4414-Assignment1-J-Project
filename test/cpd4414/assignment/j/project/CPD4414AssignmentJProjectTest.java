38a49f1cd039b5c0deb1833e9d8579b8b9fdbf43
iwuvhugs <ikirill2323@gmail.com>
2016-01-18 14:46:06 -0500

    Passed testEmptyStringShouldReturnEmptyString method.

--- test/cpd4414/assignment/j/project/CPD4414AssignmentJProjectTest.java
+++ test/cpd4414/assignment/j/project/CPD4414AssignmentJProjectTest.java
@@ -17,67 +17,40 @@ import static org.junit.Assert.*;
  * @author macbook
  */
 public class CPD4414AssignmentJProjectTest {
-    
+
     public CPD4414AssignmentJProjectTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
 
-    /**
-     * Test of main method, of class CPD4414AssignmentJProject.
-     */
     @Test
-    public void testMain() {
-        System.out.println("main");
-        String[] args = null;
-        CPD4414AssignmentJProject.main(args);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testEmptyStringShouldReturnEmptyString() {
+
+        System.out.println("testEmptyStringShouldReturnEmptyString");
+        String string = "";
+        int power = 3;
+        String expResult = "";
+        String result = CPD4414AssignmentJProject.stringPower(string, power);
+        assertEquals(expResult, result);
     }
 
-    /**
-     * Test of stringPower method, of class CPD4414AssignmentJProject.
-     */
     @Test
-    public void testStringPower() {
-        System.out.println("stringPower");
-        String string = "";
-        int power = 0;
-        String expResult = "";
-        String result = CPD4414AssignmentJProject.stringPower(string, power);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    
-    
-    @Test
-    public void testEmptyStringShouldReturnEmptyString(){
-        
-        System.out.println("stringPower");
-        String string = "";
-        int power = 0;
-        String expResult = "";
-        String result = CPD4414AssignmentJProject.stringPower(string, power);
-        assertEquals(expResult, result);
-    }
-         @Test
-    public void testLessThanOneShouldReturnNull(){
-        
+    public void testLessThanOneShouldReturnNull() {
+
         System.out.println("stringPower");
         String string = "";
         int power = 0;
@@ -85,10 +58,10 @@ public class CPD4414AssignmentJProjectTest {
         String result = CPD4414AssignmentJProject.stringPower(string, power);
         assertEquals(expResult, result);
     }
-        
-         @Test
-    public void testStringAndOneShouldReturnString(){
-        
+
+    @Test
+    public void testStringAndOneShouldReturnString() {
+
         System.out.println("stringPower");
         String string = "String";
         int power = 1;
@@ -96,23 +69,13 @@ public class CPD4414AssignmentJProjectTest {
         String result = CPD4414AssignmentJProject.stringPower(string, power);
         assertEquals(expResult, result);
     }
-        
-//        String string="";
-//        int power = 0;
-    
-//    if(string.length()==0){
-//        System.out.println("This a is "+string);
-//        }else if(power==0){
-//        System.out.println("This a is "+power);
-//        }
-    
+
     /**
      *
      */
-        
-       @Test
-    public void testStringAndTwoShouldReturnStringDoubled(){
-        
+    @Test
+    public void testStringAndTwoShouldReturnStringDoubled() {
+
         System.out.println("stringPower");
         String string = "Bob";
         int power = 2;
@@ -120,4 +83,7 @@ public class CPD4414AssignmentJProjectTest {
         String result = CPD4414AssignmentJProject.stringPower(string, power);
         assertEquals(expResult, result);
     }
-    }
+    
+    
+
+}
